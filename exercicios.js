9995253ddc4f6ab575532308ab7c41542c4451d2
LUCAS MARTINS DOS SANTOS <48139612820@imaccommand026.local>
2025-11-19 11:21:00 -0300

    exercicios 01 javascript solo

---
+++ exercicios.js
@@ -0,0 +1,23 @@
+let usuario = prompt(" Digite seu nome de usuario")
+switch (usuario) {
+    case "admin":
+        console.log("usuario correto")
+        break;
+
+
+    default:
+        console.log("usuario inciorreto")
+        break;
+}
+
+let senha = prompt("Digite sua senha")
+
+switch (senha) {
+    case ("senha123"):
+        console.log("senha correta bem vindo")
+        break;
+
+    default:
+        console.log("senha incorreta")
+        break;
+}
